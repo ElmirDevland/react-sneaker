c12e9c257190c19c9d99710e273efc34dfdaf88e
Elmir <elmir@whatever.com>
2023-06-29 19:42:44 +0400

    header create

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-45">
+        <div className="d-flex align-center">
+          <img src="/img/logo.svg" alt="logo" />
+          <div className="ml-15">
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li>
+            <img src="/img/icons/cart.svg" alt="logo" />
+
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/img/icons/user.svg" alt="logo" />
+          </li>
+        </ul>
+      </header>
+
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
+    </div>
+  );
 }
 
 export default App;
